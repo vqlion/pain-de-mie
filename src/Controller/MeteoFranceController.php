841e5e3487310889ce50c11a7f0c387c5149ba31
vqlion <valentin.jossic@insa-lyon.fr>
2025-05-11 17:45:07 +0200

    Meteo from json

--- src/Controller/MeteoFranceController.php
+++ src/Controller/MeteoFranceController.php
@@ -8,9 +8,22 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class MeteoFranceController extends AbstractController
 {
+
+    private array $meteoList;
+    private string $color = "7D85E2";
+
+    public function __construct()
+    {
+        $meteoListJson = file_get_contents('../public/static/json/meteo_france.json');
+        $this->meteoList = json_decode($meteoListJson, true);
+        $this->meteoList = array_map(fn($value) => $value . $this->color, $this->meteoList);
+    }
+
     #[Route('/meteo', name: 'app_meteo_france')]
     public function index(): Response
     {
-        return $this->render('meteo_france/index.html.twig');
+        return $this->render('meteo_france/index.html.twig', [
+            'data' => $this->meteoList
+        ]);
     }
 }
